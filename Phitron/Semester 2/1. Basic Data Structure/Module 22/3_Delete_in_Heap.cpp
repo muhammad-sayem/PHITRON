86c1cde4b16c37e06f5dcdf9621ed882b916eecc
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-18 18:02:34 +0600

    first commit

--- Phitron/Semester 2/1. Basic Data Structure/Module 22/3_Delete_in_Heap.cpp
+++ Phitron/Semester 2/1. Basic Data Structure/Module 22/3_Delete_in_Heap.cpp
@@ -31,9 +31,88 @@ void insertHeap(vector<int> &v, int x){
         }
 }
 
+void deleteHeap(vector<int> &v){
+    v[0] = v[v.size()-1];
+    v.pop_back();
+
+    int cur_ind = 0;
+
+    while(true){
+        int left_ind = (cur_ind*2)+1;
+        int right_ind = (cur_ind*2)+2;
+        int last_ind = v.size()-1;
+
+        if(left_ind <= last_ind && right_ind <= last_ind){
+            // duitai ase //
+
+            if(v[left_ind] >= v[right_ind] && v[left_ind] > v[cur_ind]){
+                swap(v[left_ind], v[cur_ind]);
+                cur_ind = left_ind;
+            }
+            else if(v[right_ind] >= v[left_ind] && v[right_ind] > v[cur_ind]){
+                swap(v[right_ind], v[cur_ind]);
+                cur_ind = right_ind;
+            }
+            else{
+                break;
+            }
+        }
+
+        else if(left_ind <= last_ind){
+            // left ase //
+
+            if(v[left_ind] > v[cur_ind]){
+                swap(v[left_ind], v[cur_ind]);
+                cur_ind = left_ind;
+            }
+            else{
+                break;
+            }
+        }
+
+        else if(right_ind <= last_ind){
+            // right ase //
+
+            if(v[right_ind] > v[cur_ind]){
+                swap(v[right_ind], v[cur_ind]);
+                cur_ind = right_ind;
+            }
+            else{
+                break;
+            }
+        }
+
+        else{
+            break;
+        }
+    }
+}
+
+void printHeap(vector<int> v){
+    for(int val: v){
+        cout << val << " "; 
+    }
+    cout << endl;
+}
+
 int main()
 {
+    vector<int> v;
 
+    int n, i;
+    cin >> n;
+
+    for(i=0; i<n; i++){
+        int x;
+        cin >> x;
+        insertHeap(v, x);
+    }
+
+    deleteHeap(v);
+    printHeap(v);
     
+    deleteHeap(v);
+    printHeap(v);
+
     return 0;
 }
