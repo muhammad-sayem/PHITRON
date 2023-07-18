d6f396c42ad11ee87863a80bb2b942bd794b5bd6
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-07-18 16:21:06 +0600

    first commit

--- Phitron/Semester 2/1. Basic Data Structure/Module 22/2_insert_in_Min_heap.cpp
+++ Phitron/Semester 2/1. Basic Data Structure/Module 22/2_insert_in_Min_heap.cpp
@@ -1,5 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void insertHeap(vector<int> &v, int x){
+
+    v.push_back(x);
+    int cur_ind = v.size()-1;
+    
+    while(cur_ind != 0){
+        
+            int parent_ind = (cur_ind-1)/2;
+
+            if(v[parent_ind] >= v[cur_ind]){
+                break;
+            }
+
+            else if(v[parent_ind] < v[cur_ind]){        // child index er value boro holei swap hoy //
+                swap(v[parent_ind], v[cur_ind]);
+            }
+            cur_ind = parent_ind;
+
+            /*
+            if(v[parent_ind] < v[cur_ind]){
+                swap(v[parent_ind], v[cur_ind]);
+            }
+            else{
+                break;
+            }
+            cur_ind = parent_ind;
+            */
+        }
+}
+
 int main()
 {
 
@@ -41,7 +72,7 @@ int main()
     }
 
     for(int val: v){
-        cout << val << " ";         // 55 50 45 30 40 42 32 25 20 10 35 //
+        cout << val << " ";        
     }
 
     return 0;
