92b44f56599e5ee387daa673cfb2349c9e506edf
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-09-13 16:48:35 +0600

    first commit

---
+++ Phitron/Semester 2/2. Introduction to Algorithm/Module 18/3_Union_by_Size.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int parent[1000];
+int parentSize[1000];
+
+void dsu_set(int n){
+    for(int i=1; i<=n; i++){
+        parent[i] = -1;
+        parentSize[i] = 1;
+    }
+}
+
+int findLeader(int node){
+    while(parent[node] != -1){
+        node = parent[node];
+    }
+    return node;
+}
+
+void dsu_union(int a, int b){
+    int leaderA = findLeader(a);
+    int leaderB = findLeader(b);
+
+    if(leaderA != leaderB){
+
+        if(leaderA > leaderB){
+            // A leader hoise // 
+            parent[leaderB] = leaderA; 
+            parentSize[leaderA] += parentSize[leaderB];
+        }
+
+        else{
+            // B leader hoise // 
+            parent[leaderA] = leaderB; 
+            parentSize[leaderB] += parentSize[leaderA];
+        }
+
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    dsu_set(n);
+
+    while(e--){
+        int a, b;
+        cin >> a >> b;
+
+        dsu_union(a, b);
+    }
+    cout << findLeader(5) << endl;
+    
+    return 0;
+}
+
+
