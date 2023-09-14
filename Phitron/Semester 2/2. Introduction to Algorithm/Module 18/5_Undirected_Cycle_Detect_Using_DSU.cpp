370592d5b59d97851d3ee5f123d8f6984a078386
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-09-14 12:51:19 +0600

    first commit

--- Phitron/Semester 2/2. Introduction to Algorithm/Module 18/5_Undirected_Cycle_Detect_Using_DSU.cpp
+++ Phitron/Semester 2/2. Introduction to Algorithm/Module 18/5_Undirected_Cycle_Detect_Using_DSU.cpp
@@ -51,30 +51,17 @@ int main()
     while(e--){
         int a, b;
         cin >> a >> b;
-
-        dsu_union(a, b);
+        int leaderA = findLeader(a);
+        int leaderB = findLeader(b);
+        if(leaderA == leaderB){
+            cout << "Cycle detected between: " << a << " " << b << endl;
+        }
+        else{
+            dsu_union(a, b);
+        }
     }
-
-    // for(int i=1; i<=n; i++){
-    //     cout << parentLevel[i] << " ";
-    // }
-    // cout << endl;
-
-    // for(int i=1; i<=n; i++){
-    //     cout << parent[i] << " ";
-    // }
-    
-    cout << findLeader(2) << endl;
     
     return 0;
 }
 
-/* 
-   --> Leader ekjon hoilei hoise, union by size othoba union by rank er khetre 
-   same input e dui jaygay dui rokom leader ashte pare, eita somossa nai.
-
-   --> DSU er khetre union by size othoba union by rank diye korte hoy....
-   karon ei duitar complexity onek valo. Noraml union er complexty er chaite //
-
-*/
 
--- Phitron/Semester 2/2. Introduction to Algorithm/Module 18/6_CSES_1666_Building_Roads_using_DSU.cpp
+++ Phitron/Semester 2/2. Introduction to Algorithm/Module 18/6_CSES_1666_Building_Roads_using_DSU.cpp
@@ -1,77 +1,77 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int parent[1000];
-int parentSize[1000];
-
+ 
+int parent[100005];
+int parentSize[100005];
+ 
 void dsu_set(int n){
     for(int i=1; i<=n; i++){
         parent[i] = -1;
         parentSize[i] = 1;
     }
 }
-
+ 
 int findLeader(int node){
     while(parent[node] != -1){
         node = parent[node];
     }
     return node;
 }
-
+ 
 void dsu_union(int a, int b){
     int leaderA = findLeader(a);
     int leaderB = findLeader(b);
-
+ 
     if(leaderA != leaderB){
-
+ 
         if(parentSize[leaderA] > parentSize[leaderB]){
-            // A leader hoise // 
             parent[leaderB] = leaderA; 
             parentSize[leaderA] += parentSize[leaderB];
         }
-
+ 
         else{
-            // B leader hoise // 
             parent[leaderA] = leaderB; 
             parentSize[leaderB] += parentSize[leaderA];
         }
-
     }
 }
-
+ 
 int main()
 {
-    int n, e;
+    int n, e, i;
     cin >> n >> e;
     dsu_set(n);
-
+ 
     while(e--){
         int a, b;
         cin >> a >> b;
-
+ 
         dsu_union(a, b);
     }
+ 
+    set<int> s;
 
-    for(int i=1; i<=n; i++){
-        cout << parentSize[i] << " ";
+    // for(i=1; i<=n; i++){
+    //     int leader = findLeader(i);
+    //     cout << "Leader of " << i << ": " << leader << endl;
+    // }
+ 
+    for(i=1; i<=n; i++){
+        int leader = findLeader(i);
+        s.insert(leader);
     }
-    cout << endl;
-
-    for(int i=1; i<=n; i++){
-        cout << parent[i] << " ";
+ 
+    vector<int> v;
+ 
+    for(auto it = s.begin(); it != s.end(); it++){
+        v.push_back(*it);
     }
     
+    cout << v.size()-1 << endl;
+ 
+    for(i=0; i<v.size()-1; i++){
+        cout << v[i] << " " << v[i+1] << endl;
+    }
+ 
     return 0;
 }
-
-/* 
-   --> Leader ekjon hoilei hoise, union by size othoba union by rank er khetre 
-   same input e dui jaygay dui rokom leader ashte pare, eita somossa nai.
-
-   --> DSU er khetre union by size othoba union by rank diye korte hoy....
-   karon ei duitar complexity onek valo. Noraml union er complexty er chaite //
-
-*/
-
-
-
