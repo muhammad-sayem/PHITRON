2a724935b70053e6c4af784468b4c984220ccb4f
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-09-16 22:30:09 +0600

    first commit

---
+++ Phitron/Semester 2/2. Introduction to Algorithm/Module 21/3_Fibonacci_using_Simple_Loop.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define  ll  long long
+using namespace std;
+int main()
+{
+    ll n, i;
+    cin >> n;
+    ll a[n+5];
+
+    a[0] = 1;
+    a[1] = 1;
+
+    for(i=2; i<=n; i++){
+        a[i] = a[i-1] + a[i-2];
+    }
+
+    cout << a[n] << endl;
+
+    
+    return 0;
+}
