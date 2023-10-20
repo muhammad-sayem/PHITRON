db8c9d9156b06a7f1fb2250c76c53c751e3b687e
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-10-20 19:28:10 +0600

    first commit

---
+++ Phitron/XPSC/Class - 04 (Bit Manipulation 2)/Codeforces_We_Neded_the_Zero.cpp
@@ -0,0 +1,51 @@
+// Multiple Answers Possible // 
+
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+        int a[n+5];
+
+        for(i=0; i<n; i++){
+            cin >> a[i];
+        }
+
+        int ans = 0;
+
+        for(i=0; i<n; i++){
+            ans = ans^a[i];
+        }
+
+        if(n%2 != 0){
+            cout << ans << endl;
+        }
+
+        else{
+            if(ans == 0) cout << 0 << endl;         // Any value is correct //
+            else cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
