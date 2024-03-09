6cb939ab0622f77b654e30efa4a4ce202fd6edbd
muhammad-sayem <shahrulislamsayem@gmail.com>
2024-03-09 22:44:33 +0600

    first commit

---
+++ Phitron/Semester 2/2. Introduction to Algorithm/Module 27/8_test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define    ll    long long
+#define    nl    '\n'
+#define    pb    push_back
+using namespace std;
+int main()
+{
+    cout << "Checking" << endl;
+    
+    return 0;
+}
