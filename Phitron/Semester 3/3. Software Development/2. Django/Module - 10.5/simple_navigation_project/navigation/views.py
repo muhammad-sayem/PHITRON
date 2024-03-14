2245e211d7ec643fb32152c7e177c23fc9c36992
muhammad-sayem <shahrulislamsayem@gmail.com>
2024-03-14 22:28:40 +0600

    first commit

--- Phitron/Semester 3/3. Software Development/2. Django/Module - 10.5/simple_navigation_project/navigation/views.py
+++ Phitron/Semester 3/3. Software Development/2. Django/Module - 10.5/simple_navigation_project/navigation/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def about(request):
-    return render(request,'navigation/about.html')
+    return render(request,'navigation/about.html')      # templates er vitorer folder er sathe eikhaner path lekhar somporko
 
 def contact(request):
     return render(request,'navigation/contact.html')
--- Phitron/Semester 3/3. Software Development/2. Django/Module - 10.5/simple_navigation_project/simple_navigation_project/urls.py
+++ Phitron/Semester 3/3. Software Development/2. Django/Module - 10.5/simple_navigation_project/simple_navigation_project/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('navigation/', include('navigation.urls')),
+    path('nav/', include('navigation.urls')),
     path('', views.home),
 ]
