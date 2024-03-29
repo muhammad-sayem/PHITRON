8320ec69be9af6edd1068617c5d2e269dcbd2132
muhammad-sayem <shahrulislamsayem@gmail.com>
2024-03-29 16:51:03 +0600

    first commit

--- Phitron/Semester 3/3. Software Development/2. Django/Module - 16 (Assignment - 4)/task_management_project/task_management_project/views.py
+++ Phitron/Semester 3/3. Software Development/2. Django/Module - 16 (Assignment - 4)/task_management_project/task_management_project/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from tasks.models import Task
 
 def home(request):
     return render(request,'home.html')
 
 def show_tasks(request):
-    return render(request, 'show_tasks.html')
+    data = Task.objects.all()
+    return render(request, 'show_tasks.html', {'data': data})
--- Phitron/Semester 3/3. Software Development/2. Django/Module - 16 (Assignment - 4)/task_management_project/tasks/views.py
+++ Phitron/Semester 3/3. Software Development/2. Django/Module - 16 (Assignment - 4)/task_management_project/tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
@@ -8,7 +9,23 @@ def add_task(request):
         task_form = TaskForm(request.POST)
         if task_form.is_valid():
             task_form.save()
-            redirect('homepage')
+            return redirect('homepage')
     else:
         task_form = TaskForm()
     return render(request, 'add_task.html', {'form': task_form})
+
+def edit_task(request, id):
+    task = Task.objects.get(pk = id)
+    task_form = TaskForm(instance = task)
+
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST, instance = task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('homepage')
+        
+    return render(request, 'add_task.html', {'form': task_form})
+
+def delete_task(request, id):
+    task = Task.objects.get(pk = id).delete()
+    return redirect('homepage')
